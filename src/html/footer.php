83f0334c48794c1e00463f46f3712de7c8b7934d
Bancquart <bancquart.v@gmail.com>
2015-12-22 16:45:49 +0100

    Footer_Header
    Ajout des composantes html/css/img du header et du footer

---
+++ src/html/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/footer.css">
+	<title>Footer</title>
+</head>
+<body>
+	<div class="background">
+		<section id="res_sociaux">
+			<a href=""><img src="img/footer/twitter.png" alt="" id="twit"></a>
+			<a href=""><img src="img/footer/facebook.png" alt="" id="fb"></a>
+			<a href=""><img src="img/footer/web.png" alt="" id="rss"></a>
+			<img src="img/footer/animal.png" alt="">
+			<a href=""><img src="img/footer/linkedin.png" alt="" id="link"></a>
+			<a href=""><img src="img/footer/viadeo.png" alt="" id="via"></a>
+			<a href=""><img src="img/footer/youtube.png" alt="" id="you"></a>
+		</section>
+		<section id="plan_site">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Campus</a></li>
+				<li><a href="">Bon Plan</a></li>
+				<li><a href="">Life Style</a></li>
+				<li><a href="">Culture</a></li>
+				<li><a href="">Evenement</a></li>
+			</ul>
+		</section>
+		<section id="mentions">
+			<ul>
+				<li><a href="">A Propos</a></li>
+				<li><a href="">Mentions Légales</a></li>
+				<li><a href="">Nous contacter</a></li>
+			</ul>
+		</section>
+	</div>
+</body>
+</html>
