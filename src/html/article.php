75fa576aa2c8d4992a2abb3a9d28a7ed73cfd2e0
Bancquart <bancquart.v@gmail.com>
2016-01-18 14:01:12 +0100

    Ajout article en modif
    Ajout du css et html de la page article - modification en cours de
    traitement.

---
+++ src/html/article.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Article</title>
+	<link rel="stylesheet" href="css/article.css">
+</head>
+<body>
+<header>
+	<?php
+		include("header.php");
+	?>
+</header>
+
+<div id="background">
+	<section id="article">
+		<h1></h1>
+		<div id="auteur">
+			<p></p>
+			<img src="" alt="Photo de profil">
+			<p></p>
+		</div>
+		<p>
+			<div class="emplacementA"></div>
+			<div class="emplacementB"></div>
+		</p>	
+	</section>
+</div>
+	<section id="note_commentaire">
+		
+	</section>
+	<section id="flux_notes_commentaires">
+		
+	</section>
+
+
+<footer>
+	<?php
+		include("footer.php");
+	?>
+</footer>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+</body>
+</html>
