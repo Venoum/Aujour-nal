4ba1764bfc4a3bbf9eaa5d01d31df71ea763de1d
Bancquart <bancquart.v@gmail.com>
2016-01-18 14:22:01 +0100

    Article modif 2
    Erreur d'import sur le dossier git -> rectification

--- src/html/article.php
+++ src/html/article.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Article</title>
-	<link rel="stylesheet" href="css/article.css">
+	<link rel="stylesheet" href="../css/article.css">
+	<link rel="stylesheet" href="../css/article_mod1.css">
 </head>
 <body>
 <header>
@@ -11,27 +12,36 @@
 		include("header.php");
 	?>
 </header>
-
 <div id="background">
 	<section id="article">
-		<h1></h1>
-		<div id="auteur">
-			<p></p>
-			<img src="" alt="Photo de profil">
-			<p></p>
-		</div>
-		<p>
-			<div class="emplacementA"></div>
-			<div class="emplacementB"></div>
-		</p>	
+		<h1>Lorem ipsum dolor sit amet,  sit  amet consectetur.</h1>
+		<div id="corpus">	<!--C'est du css la mais on voit avec symphony pour les données ? (script JS sur le src de img?)-->
+			<p>Ludovic BOUVINET</p>
+			<img id="photo_profil" src="img/article/ludo.png" alt="Photo de profil">
+			<p class="grade">Rédacteur en chef</p> 
+		
+			<p id="chapeau">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p class="pg1">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+			</p>
+			<p class="pg2">
+				&nbsp;&nbsp;&nbsp;Sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed 
+				<span id="emplacementA">« Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam »</span>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ex ea commodo consequat. Duis aute irure in dolor in adipisci.
+				<img id="emplacementB" src="img/article/times-square.jpg" alt="">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaeratLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			</p>
+		</div>	
 	</section>
 </div>
-	<section id="note_commentaire">
+<section id="note_commentaire">
 		
-	</section>
-	<section id="flux_notes_commentaires">
+</section>
+<section id="flux_notes_commentaires">
 		
-	</section>
+</section>
 
 
 <footer>
